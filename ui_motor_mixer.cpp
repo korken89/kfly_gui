c4d4d558b703187d95ae16b70ac3833f831a2ac0
Emil Fresk <emil.fresk@gmail.com>
2017-05-31 21:53:43 +0200

    Added mixer channels

--- ui_motor_mixer.cpp
+++ ui_motor_mixer.cpp
@@ -2,10 +2,24 @@
 #include "ui_ui_motor_mixer.h"
 
 ui_motor_mixer::ui_motor_mixer(QWidget *parent) :
-  QWidget(parent),
-  ui(new Ui::ui_motor_mixer)
+    QWidget(parent),
+    ui(new Ui::ui_motor_mixer)
 {
-  ui->setupUi(this);
+    ui->setupUi(this);
+
+    for (int i = 0; i < 8; i++)
+    {
+        ui_motor_mixer_channel *chan = new ui_motor_mixer_channel;
+        _channels.push_back(chan);
+
+        //chan->set_channel_number(i+1);
+
+        /* Connect each channel to know when any value has changed. */
+        //connect(chan, &ui_rc_output_channel::value_changed,
+        //        this, &ui_rc_output::channel_value_changed);
+
+        ui->layoutChannels->addWidget(chan);
+    }
 }
 
 ui_motor_mixer::~ui_motor_mixer()
--- ui_motor_mixer.h
+++ ui_motor_mixer.h
@@ -2,21 +2,33 @@
 #define UI_MOTOR_MIXER_H
 
 #include <QWidget>
+#include <QTimer>
+
+#include "communication.h"
+#include "ui_motor_mixer_channel.h"
 
 namespace Ui {
-  class ui_motor_mixer;
+    class ui_motor_mixer;
 }
 
 class ui_motor_mixer : public QWidget
 {
-  Q_OBJECT
+    Q_OBJECT
 
 public:
-  explicit ui_motor_mixer(QWidget *parent = 0);
-  ~ui_motor_mixer();
+    explicit ui_motor_mixer(QWidget *parent = 0);
+    ~ui_motor_mixer();
 
 private:
-  Ui::ui_motor_mixer *ui;
+    Ui::ui_motor_mixer *ui;
+
+    communication *_communication;
+    std::vector<ui_motor_mixer_channel *> _channels;
+
+    bool _upload_settings;
+    QTimer _upload_settings_timer;
+
+    void upload_settings();
 };
 
 #endif // UI_MOTOR_MIXER_H
